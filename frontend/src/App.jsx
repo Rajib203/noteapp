85631dd150f9a8b3996e0234db77cbb8ffb40a39
Rajib203 <skrajibuddin472@gmail.com>
2025-10-23 19:41:35 +0530

    fibnal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import CreateNote from './pages/createNote'
+import CreateNote from './pages/CreateNote'
 function App() {
   return (
     <div className='flex flex-col min-h-screen bg-gray-700 text-white'>
